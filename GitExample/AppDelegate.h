596975736c15a92a882637f22e5dcc6803832850
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-12 12:50:17 +0530

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by mTL NARESH on 12/10/18.
+//  Copyright © 2018 mTL NARESH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitExample
+//
+//  Created by mTL NARESH on 12/10/18.
+//  Copyright © 2018 mTL NARESH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
