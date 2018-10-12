2fce6ea50820e7c1fce32ec432f0831e0d7be1e9
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-12 12:55:10 +0530

    added sum and multiplication functions

--- GitExample/ViewController.m
+++ GitExample/ViewController.m
@@ -19,5 +19,14 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)handleSumOfNo:(id)sender {
+    
+    int a = 10,b = 20;
+    NSLog(@"sum is %d",a+b);
+}
+- (IBAction)handleMulOfNo:(id)sender {
+    int a = 10,b = 20;
+    NSLog(@"Mul is %d",a*b);
+}
 
 @end
